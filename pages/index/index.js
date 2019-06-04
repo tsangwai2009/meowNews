ea7b0de0789bfaa580c65be9e4937d472c63ba65
waitsang <tsangwai2009@gmail.com>
2019-06-04 20:01:19 +0800

    Finished HomePage and DetailPage

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,28 +1,42 @@
-const dayMap = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+
 Page({
   data: {
+    kindItem:[1,2,3,4,5,6,7],
+    newsItemGn:[],
+    newsItemGj:[],
+    newsItemCj:[],
+    newsItemYl:[],
+    newsItemJs:[],
+    newsItemTy:[],
+    newsItemOther:[],
     clientHeight:'',
-    newsItem: [],
+    kind: ["gn","gj","cj","yl","js","ty","other"],
+    tabs: [{ kind: "國內" }, { kind: "國際" }, { kind: "財經" }, 
+      { kind: "娛樂" }, { kind: "軍事" }, { kind: "體育" },
+      { kind: "其他" }],
     currentTab: 0
   },
   onLoad(options)
-   {
-    this.setNews();
-        var that = this 
+  {
+    for (let i = 0; i < 7; i++) { this.getNews(this.data.kind[i])}
+    var that = this 
     wx.getSystemInfo({ 
         success: function (res) { 
             that.setData({ 
                 clientHeight: res.windowHeight
             }); 
            console.log(res.windowHeight)
-        } 
-    })
+        } })
+  },
+  onPullDownRefresh()
+  { 
+    for (let i = 0; i < 7; i++) { this.getNews(this.data.kind[i], () => { wx.stopPullDownRefresh(); }) }
   },
   //滑动切换
   swiperTab(e) {
     this.setData({
       currentTab: e.detail.current
-    });
+    })
   },
   //点击切换
   clickTab(e) {
@@ -34,19 +48,44 @@ Page({
       })
     }
   },
-  setNews() {
+  getNews(code,callback){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: { "type": code },
+      success: res => {
+        let result = res.data.result;
+        console.log(result);
+        this.setNews(result,code)
+      },
+      complete: () => { callback && callback() }//check function parameter exist
+    })
+  },
+  setNews(result,code) 
     {
-      let newsItem = []
-      for (let i = 0; i < 18; i++) {
-        let date = new Date()
-        date.setDate(date.getDate() + i)
+      let newsItem = [] //anytype
+      for (let i = 0; i <result.length; i++) {
         newsItem.push(//push items to array element
           {
-            time: dayMap[date.getDay()],
-            content: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`,
+            id:result[i].id,
+            title: result[i].title,
+            date: result[i].date,
+            source: result[i].source,
+            firstImage: result[i].firstImage
+
           }
         )
-      } this.setData({ newsItem: newsItem })
+      } 
+    if (code=="gn") this.setData({ newsItemGn: newsItem})
+    if (code == "gj") this.setData({ newsItemGj: newsItem })
+    if (code == "cj") this.setData({ newsItemCj: newsItem })
+    if (code == "yl") this.setData({ newsItemYl: newsItem })
+    if (code == "js") this.setData({ newsItemJs: newsItem })
+    if (code == "ty") this.setData({ newsItemTy: newsItem })
+    if (code == "other") this.setData({ newsItemOther: newsItem })
+    },
+    newsDetail: function(event) {
+      wx.navigateTo({
+        url: '/pages/detail/detail?id=' + event.currentTarget.dataset.id,
+      })
     }
-  }
 })
